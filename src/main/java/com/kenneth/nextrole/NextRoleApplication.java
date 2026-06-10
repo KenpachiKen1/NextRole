c8e04ea4ec298176b6b40914e83d8c4d3ae7eaeb
KenpachiKen1 <kentimi35@gmail.com>
2026-06-10 18:25:22 -0500

    First commit

---
+++ src/main/java/com/kenneth/nextrole/NextRoleApplication.java
@@ -0,0 +1,13 @@
+package com.kenneth.nextrole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //telling app its a springboot project
+public class NextRoleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NextRoleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kenneth/nextrole/NextRoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kenneth.nextrole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextRoleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
