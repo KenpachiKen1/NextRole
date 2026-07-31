3e2dc67036e36091e9bea81b72c79fa21ce5c730
KenpachiKen1 <kentimi35@gmail.com>
2026-07-31 02:03:50 -0500

    added calendar, going to add status labels (enums) and then work on job posting/entry.

--- Frontend/nextrole-ui/src/app/pages/calendar/calendar.ts
+++ Frontend/nextrole-ui/src/app/pages/calendar/calendar.ts
@@ -3,9 +3,11 @@ import { CalendarService } from '../../services/calendarService';
 import { CalendarDay } from '../../models/calendar.models';
 import { Button } from '../../components/global/button/button';
 import { DatePipe } from '@angular/common';
+import { NgClass } from '@angular/common';
+
 @Component({
   selector: 'app-calendar',
-  imports: [Button, DatePipe],
+  imports: [Button, DatePipe, NgClass],
   templateUrl: './calendar.html',
   styleUrl: './calendar.css',
 })
@@ -18,12 +20,14 @@ export class Calendar implements OnInit {
   month = signal(this.today.getMonth());
 
   displayDate = computed(() => new Date(this.year(), this.month(), 1));
-  
+
   grid = signal<CalendarDay[]>([]);
 
   ngOnInit() {
     this.loadMonth();
   }
+
+  daysOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
   nextMonth() {
     if (this.month() === 11) {
